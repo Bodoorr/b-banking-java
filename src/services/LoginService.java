8e2fc4a4b087e9f55432c32db5ef1acf52ff8fee
Bodoorr <bodoor5@hotmail.com>
2026-09-14 12:47:47 +0300

    update the LoginService to read the id and password from the data.txt file

--- src/services/LoginService.java
+++ src/services/LoginService.java
@@ -1,10 +1,26 @@
 package services;
 import models.User;
 import java.util.*;
+import java.io.*;
+import utils.FileManager;
 public class LoginService implements Authentication{
 
     @Override
     public Optional<User> login(String id, String password) {
+        try {
+            List<String> users = FileManager.readAllLines("data.txt");
+
+            for (String user:users){
+                String [] data = user.split(",");
+                if (data[0].equals(id) && data[3].equals(password)){
+                    System.out.println("Login Successfully!");
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
         return Optional.empty();
     }
+
 }
