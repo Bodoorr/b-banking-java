0d71f3f02f0be6beb7e9408e7ddc8272713da7e1
Bodoorr <bodoor5@hotmail.com>
2026-09-14 21:01:16 +0300

    update LoginService to use correct data fields.

--- src/services/LoginService.java
+++ src/services/LoginService.java
@@ -15,15 +15,14 @@ public class LoginService implements Authentication{
             for (String user:users){
                 String [] data = user.split(",");
                 if (data[0].equals(id) && data[3].equals(password)){
-                    double balance= Double.parseDouble(data[4]);
-                    if(data[5].equals("C")){
+                    if(data[4].equals("C")){
                         Customer customer=new Customer(
-                                data[0],data[1],data[2],data[3],data[5]
+                                data[0],data[1],data[2],data[3],data[4]
                         );
                         return Optional.of(customer);
-                    } else if(data[5].equals("B")){
+                    } else if(data[4].equals("B")){
                         Banker banker=new Banker(
-                                data[0],data[1],data[2],data[3],data[5]
+                                data[0],data[1],data[2],data[3],data[4]
                         );
                         return Optional.of(banker);
                     }
