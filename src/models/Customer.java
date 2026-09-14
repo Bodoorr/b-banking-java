bbc42fabf7f4568382f330122163c9d28bb57c8d
Bodoorr <bodoor5@hotmail.com>
2026-09-14 14:56:15 +0300

    update the customer class to add account for customer.

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -1,7 +1,19 @@
 package models;
+import java.util.ArrayList;
 
 public class Customer extends User {
     public Customer(String id, String firstName, String lastName, String password, String role){
         super(id, firstName, lastName, password, role);
     }
+
+    private ArrayList<Account> accounts = new ArrayList<>();
+
+    public void addAccount(Account newAccount){
+        accounts.add(newAccount);
+    }
+
+    public ArrayList<Account> getAccounts(){
+        return accounts;
+    }
+
 }
