7209541a4ff2296455dba784703b16e2f68e2b45
Bodoorr <bodoor5@hotmail.com>
2026-09-15 00:33:17 +0300

    add overdraft logic to withdraw method in Account class.

--- src/models/Account.java
+++ src/models/Account.java
@@ -3,11 +3,14 @@ package models;
 public abstract class Account {
     private double balance;
     private String accountId;
+    private int overdraftCount;
+    private boolean accountActive;
 
-
-    public Account(String accountId, double balance){
+    public Account(String accountId, double balance, int overdraftCount, boolean accountActive){
         this.accountId= accountId;
         this.balance= balance;
+        this.overdraftCount= overdraftCount;
+        this.accountActive= accountActive;
     }
 
     public String getAccountId(){
@@ -25,7 +28,34 @@ public abstract class Account {
 
     //withdraw
     public void withdraw(double amount){
+        if (!accountActive){
+            System.out.println("Account is deactivated.");
+            return;
+        }
+
+        if (balance<0 && amount>100){
+            System.out.println("Decline, You can't make this request.");
+            return;
+        }
         balance -= amount;
+
+        if (balance<0){
+            balance-=35;
+            overdraftCount++;
+
+            if (overdraftCount>=2){
+                accountActive= false;
+            }
+        }
+
+    }
+
+    public int getOverdraftCount(){
+        return overdraftCount;
+    }
+
+    public boolean getAccountActive(){
+        return accountActive;
     }
 
 }
