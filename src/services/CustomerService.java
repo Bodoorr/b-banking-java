626b652f7ac5eac7fb4db128609efa06bb26f416
Bodoorr <bodoor5@hotmail.com>
2026-09-14 20:38:35 +0300

    adding addCustomer method in the CustomerService class.

---
+++ src/services/CustomerService.java
@@ -0,0 +1,42 @@
+package services;
+
+import models.CheckingAccount;
+import models.Customer;
+import models.SavingsAccount;
+import utils.FileManager;
+import java.util.ArrayList;
+
+public class CustomerService {
+    public void addCustomer(String id, String firstName, String lastName, String password, String accountType, double checkingAccountBalance , double savingsAccountBalance, String checkingAccountId, String savingsAccountId){
+        Customer customer=new Customer(id,firstName,lastName, password, "C");
+        String customerData= id+","+firstName+","+lastName+","+password+","+"C";
+        ArrayList<String> customerList= new ArrayList<>();
+        customerList.add(customerData);
+        FileManager.writeData("src/data", customerList);
+
+        ArrayList<String> accountList= new ArrayList<>();
+        if(accountType.equals("CHECKING")){
+        CheckingAccount checkingAccount=new CheckingAccount(checkingAccountId,checkingAccountBalance);
+        customer.addAccount(checkingAccount);
+        String accountData= checkingAccountId+","+id+","+accountType+","+checkingAccountBalance;
+        accountList.add(accountData);
+    } else if (accountType.equals("SAVINGS")) {
+        SavingsAccount savingsAccount=new SavingsAccount(savingsAccountId,savingsAccountBalance);
+        customer.addAccount(savingsAccount);
+        String accountData= savingsAccountId+","+id+","+accountType+","+savingsAccountBalance;
+        accountList.add(accountData);
+        } else if (accountType.equals("BOTH")){
+        CheckingAccount checkingAccount=new CheckingAccount(checkingAccountId,checkingAccountBalance);
+        SavingsAccount savingsAccount=new SavingsAccount(savingsAccountId,savingsAccountBalance);
+        customer.addAccount(checkingAccount);
+        customer.addAccount(savingsAccount);
+        String checkingAccountData= checkingAccountId+","+id+","+"CHECKING"+","+checkingAccountBalance;
+        String savingsAccountData= savingsAccountId+","+id+","+"SAVINGS"+","+savingsAccountBalance;
+        accountList.add(checkingAccountData);
+        accountList.add(savingsAccountData);
+        }
+
+        FileManager.writeData("src/accounts", accountList);
+
+    }
+}
