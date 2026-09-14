b7caf46e8ac111280e400daf9c1a92d436b79e87
Bodoorr <bodoor5@hotmail.com>
2026-09-14 22:55:36 +0300

    added AccountService to read account data and return the accounts details.

---
+++ src/services/AccountService.java
@@ -0,0 +1,36 @@
+package services;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import models.CheckingAccount;
+import models.SavingsAccount;
+import utils.FileManager;
+import models.Account;
+public class AccountService {
+
+    public ArrayList<Account> getAccounts(String customerId){
+        ArrayList<Account> accountsList= new ArrayList<>();
+        try {
+            List<String> lines= FileManager.readAllLines("src/accounts");
+
+            for (String line:lines){
+                String [] data= line.split(",");
+                if (data[1].equals(customerId)){
+                    double balance= Double.parseDouble(data[3]);
+                    if (data[2].equals("CHECKING")){
+                        CheckingAccount checkingAccount=new CheckingAccount(data[0],balance);
+                        accountsList.add(checkingAccount);
+                    }else if(data[2].equals("SAVINGS")){
+                        SavingsAccount savingsAccount=new SavingsAccount(data[0],balance);
+                        accountsList.add(savingsAccount);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+return accountsList;
+    }
+}
--- src/utils/FileManager.java
+++ src/utils/FileManager.java
@@ -4,17 +4,17 @@ import java.io.*;
 import java.util.*;
 
 public class FileManager {
-    public static void main(String[] args) {
-
-        // Read file
-        try {
-            List<String> lines = readAllLines("src/data");
-            System.out.println("Read " + lines.size() + " lines:");
-            lines.forEach(System.out::println);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+//    public static void main(String[] args) {
+//
+//        // Read file
+//        try {
+//            List<String> lines = readAllLines("src/data");
+//            System.out.println("Read " + lines.size() + " lines:");
+//            lines.forEach(System.out::println);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     // Create file
     public static void writeData(String filename, List<String> dataEntries) {
