b7c431d92d719686836fd254f2b25da0f020d5b0
Bodoorr <bodoor5@hotmail.com>
2026-09-16 01:22:33 +0300

    create Transaction model with trasaction details

---
+++ src/models/Transaction.java
@@ -0,0 +1,46 @@
+package models;
+
+import java.time.LocalDateTime;
+
+public class Transaction{
+    private String customerId;
+    private String accountId;
+    private double balance;
+    private double amount;
+    private String transactionType;
+    private LocalDateTime date;
+
+    public Transaction(String customerId, String accountId, double balance, double amount, String transactionType, LocalDateTime date){
+        this.customerId= customerId;
+        this.accountId= accountId;
+        this.balance= balance;
+        this.amount= amount;
+        this.transactionType= transactionType;
+        this.date= date;
+    }
+
+    public String getCustomerId(){
+        return customerId;
+    }
+
+    public String getAccountId(){
+        return accountId;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public String getTransactionType(){
+        return transactionType;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+}
+
