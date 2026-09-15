131929e98b3b63debc717b2f5e8eb902b46e290d
Bodoorr <bodoor5@hotmail.com>
2026-09-16 02:47:28 +0300

    add transaction tracking in AccountService

--- src/services/AccountService.java
+++ src/services/AccountService.java
@@ -1,14 +1,17 @@
 package services;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import models.CheckingAccount;
 import models.SavingsAccount;
+import models.Transaction;
 import utils.FileManager;
 import models.Account;
 public class AccountService {
+    TransactionService transactionService = new TransactionService();
 
     public ArrayList<Account> getAccounts(String customerId){
         ArrayList<Account> accountsList= new ArrayList<>();
@@ -50,15 +53,36 @@ return accountsList;
 
                     if (data[2].equals("CHECKING")){
                         CheckingAccount checkingAccount=new CheckingAccount(data[0],balance,overdraftCount,accountActive);
+
+                        double checkingOldBalance= checkingAccount.getBalance();
                         checkingAccount.deposit(amount);
+
+                        if (checkingAccount.getBalance()==checkingOldBalance){
+                            System.out.println("Transaction failed.");
+                            return;
+                        }
+
                         String updateLine= data[0]+","+data[1]+","+data[2]+","+checkingAccount.getBalance()+","+checkingAccount.getOverdraftCount()+","+checkingAccount.getAccountActive();
                         updatedLines.add(updateLine);
+                        Transaction transaction=new Transaction(LocalDateTime.now(),data[1],accountId,checkingAccount.getBalance(),amount,"DEPOSIT");
+                        transactionService.saveTransaction(transaction);
                     }else if(data[2].equals("SAVINGS")){
                         SavingsAccount savingsAccount=new SavingsAccount(data[0],balance,overdraftCount,accountActive);
+
+                        double savingsOldBalance= savingsAccount.getBalance();
                         savingsAccount.deposit(amount);
+
+                        if (savingsAccount.getBalance()==savingsOldBalance){
+                            System.out.println("Transaction failed.");
+                            return;
+                        }
+
                         String updateLine= data[0]+","+data[1]+","+data[2]+","+savingsAccount.getBalance()+","+savingsAccount.getOverdraftCount()+","+savingsAccount.getAccountActive();
                         updatedLines.add(updateLine);
+                        Transaction transaction=new Transaction(LocalDateTime.now(),data[1],accountId,savingsAccount.getBalance(),amount,"DEPOSIT");
+                        transactionService.saveTransaction(transaction);
                     }
+
                 } else {
                     updatedLines.add(line);
                 }
@@ -83,14 +107,35 @@ return accountsList;
                     boolean accountActive= Boolean.parseBoolean(data[5]);
                     if (data[2].equals("CHECKING")){
                         CheckingAccount checkingAccount=new CheckingAccount(data[0],balance,overdraftCount,accountActive);
+
+                        double checkingOldBalance= checkingAccount.getBalance();
                         checkingAccount.withdraw(amount);
+
+                        if (checkingAccount.getBalance() == checkingOldBalance){
+                            System.out.println("Transaction failed.");
+                            return;
+                        }
+
                         String updateLine= data[0]+","+data[1]+","+data[2]+","+checkingAccount.getBalance()+","+checkingAccount.getOverdraftCount()+","+checkingAccount.getAccountActive();
                         updatedLines.add(updateLine);
+                        Transaction transaction=new Transaction(LocalDateTime.now(),data[1],accountId,checkingAccount.getBalance(),amount,"WITHDRAW");
+                        transactionService.saveTransaction(transaction);
                     }else if (data[2].equals("SAVINGS")){
                         SavingsAccount savingsAccount=new SavingsAccount(data[0],balance,overdraftCount,accountActive);
+
+                        double savingsOldBalance= savingsAccount.getBalance();
                         savingsAccount.withdraw(amount);
+
+                        if (savingsAccount.getBalance()==savingsOldBalance){
+                            System.out.println("Transaction failed.");
+                            return;
+                        }
+
                         String updateLine= data[0]+","+data[1]+","+data[2]+","+savingsAccount.getBalance()+","+savingsAccount.getOverdraftCount()+","+savingsAccount.getAccountActive();
                         updatedLines.add(updateLine);
+                        Transaction transaction=new Transaction(LocalDateTime.now(),data[1],accountId,savingsAccount.getBalance(),amount,"WITHDRAW");
+                        transactionService.saveTransaction(transaction);
+
                     }
                 }
                 else{
@@ -107,6 +152,9 @@ return accountsList;
         Account fromAccount=null;
         Account toAccount=null;
 
+        String fromCustomerId="";
+        String toCustomerId="";
+
         try {
             List<String> lines=FileManager.readAllLines("src/accounts");
             for (String line:lines){
@@ -116,6 +164,7 @@ return accountsList;
                 boolean accountActive= Boolean.parseBoolean(data[5]);
 
                 if (data[0].equals(fromAccountId)){
+                    fromCustomerId= data[1];
                     if (data[2].equals("CHECKING")){
                         fromAccount= new CheckingAccount(data[0],balance,overdraftCount,accountActive);
                     } else if (data[2].equals("SAVINGS")) {
@@ -123,6 +172,7 @@ return accountsList;
                     }
                 }
                 if(data[0].equals(toAccountId)){
+                    toCustomerId= data[1];
                     if (data[2].equals("CHECKING")){
                         toAccount=new CheckingAccount(data[0],balance,overdraftCount,accountActive);
                     }else if (data[2].equals("SAVINGS")){
@@ -136,11 +186,19 @@ return accountsList;
             }
             double oldBalance= fromAccount.getBalance();
             fromAccount.withdraw(amount);
+
             if (fromAccount.getBalance() == oldBalance){
                 System.out.println("Transfer Failed.");
                 return;
             }
+            Transaction withdrawTransaction=new Transaction(LocalDateTime.now(),fromCustomerId,fromAccountId,fromAccount.getBalance(),amount,"TRANSFER-WITHDRAW");
+            transactionService.saveTransaction(withdrawTransaction);
+
             toAccount.deposit(amount);
+
+            Transaction depositTransaction=new Transaction(LocalDateTime.now(),toCustomerId,toAccountId,toAccount.getBalance(),amount,"TRANSFER-DEPOSIT");
+            transactionService.saveTransaction(depositTransaction);
+
             ArrayList<String> updatedLines= new ArrayList<>();
             for (String line:lines){
                 String [] data= line.split(",");
