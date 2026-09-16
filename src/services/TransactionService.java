e880d0794d83002a410972009ec71c75783e0711
Bodoorr <bodoor5@hotmail.com>
2026-09-16 09:42:51 +0300

    add displayAccountStatement method in TransactionService

--- src/services/TransactionService.java
+++ src/services/TransactionService.java
@@ -1,5 +1,8 @@
 package services;
+import models.CheckingAccount;
+import models.SavingsAccount;
 import models.Transaction;
+import models.Account;
 import utils.FileManager;
 
 import java.io.IOException;
@@ -33,6 +36,37 @@ public class TransactionService {
                 System.out.println("No transactions found.");
             }else {
                 System.out.println("Total Transactions: "+transactionCount);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void displayAccountStatement(String customerId, Account account){
+        System.out.println("DETAILED ACCOUNT STATEMENT\n");
+        System.out.println();
+        if (account instanceof CheckingAccount){
+            System.out.println("Account Type: Checking");
+        } else if (account instanceof SavingsAccount){
+            System.out.println("Account Type: Savings");
+        }
+        System.out.println("Account Number: "+account.getAccountId());
+        System.out.println("Current Balance: "+account.getBalance()+" BHD");
+        System.out.println();
+        System.out.println("TRANSACTIONS");
+        System.out.println();
+
+        //read transactions
+        try {
+            List<String> lines= FileManager.readAllLines("src/transactions");
+
+            for (String line:lines){
+                String [] data= line.split(",");
+                if (data[1].equals(customerId) && data[2].equals(account.getAccountId())){
+                        String transaction= "Transaction Type: "+data[5]+"\n"+"Date: "+data[0]+"\n"+
+                                "\n"+"Amount: "+data[4]+"\n"+"Balance: "+data[3]+"\n";
+                        System.out.println(transaction);
+                }
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
