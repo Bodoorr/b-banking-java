6aab0cc8128572cfa2a7e4fd2dd1a788d3121c24
Bodoorr <bodoor5@hotmail.com>
2026-09-16 03:01:17 +0300

    add transaction displayTransaction method in TransactionService

--- src/services/TransactionService.java
+++ src/services/TransactionService.java
@@ -2,7 +2,9 @@ package services;
 import models.Transaction;
 import utils.FileManager;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class TransactionService {
     public void saveTransaction(Transaction transaction){
@@ -11,5 +13,22 @@ public class TransactionService {
         ArrayList<String> transactions=new ArrayList<>();
         transactions.add(transactionData);
         FileManager.writeData("src/transactions",transactions);
+    }
+
+
+    public void displayTransaction(String customerId){
+        try {
+            List<String> lines= FileManager.readAllLines("src/transactions");
+            for (String line:lines){
+                String [] data=line.split(",");
+                if (data[1].equals(customerId)){
+                    String transaction= "Transaction Type: "+data[5]+"\n"+"Date: "+data[0]+"\n"+"Account: "+data[2]+
+                            "\n"+"Amount: "+data[4]+"\n"+"Balance: "+data[3];
+                    System.out.println(transaction);
+                }
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
