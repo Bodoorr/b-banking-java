33c0322570e1655374314c56832c904f9bf84f0d
Bodoorr <bodoor5@hotmail.com>
2026-09-16 19:40:00 +0300

    add DebitCard model to handle Mastercard types

---
+++ src/models/DebitCard.java
@@ -0,0 +1,65 @@
+package models;
+
+public class DebitCard {
+
+    private String cardType;
+    private double withdrawLimit;
+    private double transferLimit;
+    private double ownTransferLimit;
+    private double depositLimit;
+    private double ownDepositLimit;
+
+
+    public DebitCard(String cardType){
+        this.cardType = cardType;
+        switch (cardType){
+            case "Platinum":
+                withdrawLimit= 20000;
+                transferLimit= 40000;
+                ownTransferLimit= 80000;
+                depositLimit= 100000;
+                ownDepositLimit= 200000;
+                break;
+            case "Titanium":
+                withdrawLimit= 10000;
+                transferLimit= 20000;
+                ownTransferLimit= 40000;
+                depositLimit= 100000;
+                ownDepositLimit= 200000;
+                break;
+            case "Mastercard":
+                withdrawLimit= 5000;
+                transferLimit= 10000;
+                ownTransferLimit= 20000;
+                depositLimit= 100000;
+                ownDepositLimit= 200000;
+                break;
+            default:
+            throw new IllegalArgumentException("Invalid card type.");
+        }
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    public double getWithdrawLimit() {
+        return withdrawLimit;
+    }
+
+    public double getTransferLimit() {
+        return transferLimit;
+    }
+
+    public double getOwnTransferLimit() {
+        return ownTransferLimit;
+    }
+
+    public double getDepositLimit() {
+        return depositLimit;
+    }
+
+    public double getOwnDepositLimit() {
+        return ownDepositLimit;
+    }
+}
